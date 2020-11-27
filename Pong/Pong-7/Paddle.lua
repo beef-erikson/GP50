0bc28784f1c156fd01672d9aafc802c101bd2913
Troy Martin <beef.erikson.studios@gmail.com>
2020-11-27 05:35:41 -0500

    Pong-7 complete, Collision update

---
+++ Pong/Pong-7/Paddle.lua
@@ -0,0 +1,39 @@
+--[[
+    Paddle class for handling the paddles in Pong
+
+    Author: Troy Martin
+    beef.erikson.studios@gmail.com
+]]
+
+Paddle = Class{}
+
+--[[
+    Initializes paddle with given parameters
+]]
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- Velocity of the paddle
+    self.dy = 0
+end
+
+--[[
+    Clamps the paddle to the window
+]]
+function Paddle:update(dt)
+    if self.dy < 0 then
+        self.y = math.max(0, self.y + self.dy * dt)
+    else
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+--[[
+    Renders the ball
+]]
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
