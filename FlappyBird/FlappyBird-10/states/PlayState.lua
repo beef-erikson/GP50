001c5286d136683bf2a441626437d4bc082e85df
Troy Martin <beef.erikson.studios@gmail.com>
2020-12-03 13:52:00 -0500

    Countdown Update

---
+++ FlappyBird/FlappyBird-10/states/PlayState.lua
@@ -0,0 +1,108 @@
+--[[
+    Flappy Bird Clone
+    Lua Build for Love2d
+
+    PlayState for state passed from StateMachine
+    Defines behavior and updates/rendering
+
+    Author: Troy Martin
+    beef.erikson.studios@gmail.com
+]]
+
+PlayState = Class{__includes = BaseState}
+
+PIPE_SPEED = 60
+PIPE_WIDTH = 70
+PIPE_HEIGHT = 288
+
+BIRD_WIDTH = 38
+BIRD_HEIGHT = 24
+
+--[[
+    Initialize properties
+]]
+function PlayState:init()
+    self.bird = Bird()
+    self.pipePairs = {}
+    self.timer = 0
+
+    -- scoring
+    self.score = 0
+
+    -- pipe gap
+    self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+end
+
+-- Called once per frame, handles logic
+function PlayState:update(dt)
+    -- updates pipe spawning timer
+    self.timer = self.timer + dt
+
+    -- spawn new pipe every 2 seconds
+    if self.timer > 2 then
+        local y = math.max(-PIPE_HEIGHT + 10,
+            math.min(self.lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        self.lastY = y
+
+        -- add new pipes to pipePairs
+        table.insert(self.pipePairs, PipePair(y))
+
+        -- reset timer
+        self.timer = 0
+    end
+
+    -- for every pair of pipes
+    for k, pair in pairs(self.pipePairs) do
+        -- scores point if bird has gone beyond pair pipe
+        if not pair.scored then
+            if pair.x + PIPE_WIDTH < self.bird.x then
+                self.score = self.score + 1
+                pair.scored = true
+            end
+        end
+
+        -- updates position of pair
+        pair:update(dt)
+    end
+
+    -- second loop to avoid table removal issues in first loop
+    for k, pair in pairs(self.pipePairs) do
+        if pair.remove then
+            table.remove(self.pipePairs, k)
+        end
+    end
+
+    -- update bird based on input/gravity
+    self.bird:update(dt)
+
+    -- collision detection using AABB
+    for k, pair in pairs(self.pipePairs) do
+        for l, pipe in pairs(pair.pipes) do
+            if self.bird:collides(pipe) then
+                gStateMachine:change('score', {
+                    score = self.score
+                })
+            end
+        end
+    end
+
+    -- reset if bird hits ground
+    if self.bird.y > VIRTUAL_HEIGHT - 15 then
+        gStateMachine:change('score', {
+            score = self.score
+        })
+    end
+end
+
+-- draws to screen
+function PlayState:render()
+    for k, pair in pairs(self.pipePairs) do
+        pair:render()
+    end
+
+    -- draws score to screen
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
+    self.bird:render()
+end
---
+++ FlappyBird/FlappyBird-10/states/TitleScreenState.lua
@@ -0,0 +1,28 @@
+--[[
+    Flappy Bird Clone
+    Lua Build for Love2d
+
+    TitleScreenState for state passed from StateMachine
+    Defines behavior and updates/rendering
+
+    Author: Troy Martin
+    beef.erikson.studios@gmail.com
+]]
+
+TitleScreenState = Class{__includes = BaseState}
+
+-- if return is hit, change state to play
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed(dt) or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+-- render text for title screen
+function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Flappy Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+end
